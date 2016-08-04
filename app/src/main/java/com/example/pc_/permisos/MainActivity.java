49ddf2a7f2541f598232668dc958cccc6928a072
jose1824 <jose1824@live.com.mx>
2016-08-04 08:47:03 -0500

    Permisos solo la anotacion de API y otras cositas

--- app/src/main/java/com/example/pc_/permisos/MainActivity.java
+++ app/src/main/java/com/example/pc_/permisos/MainActivity.java
@@ -1,13 +1,33 @@
 package com.example.pc_.permisos;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnSMS;
+    TextView txtSMS;
+    String[] permisos = {Manifest.permission.READ_SMS};
+    int REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSMS = (Button) findViewById(R.id.main_btn_sms);
+        txtSMS = (TextView) findViewById(R.id.main_txt_sms);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) { //Si la version del SDK del telefono es mayor o igual que M has lo que este adentro
+            if (checkSelfPermission(Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED) {
+
+            }
+        }
+
     }
 }
